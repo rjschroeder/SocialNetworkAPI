126f8f5ac8d626db82cc47cc6637076a6886b6be
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 05:55:29 -0600

    model for reaction

--- models/reaction.js
+++ models/reaction.js
@@ -1,7 +1,25 @@
 const {Schema,Types} = require('mongoose');
 
 const reactionSchema = new Schema({
-
-})
+    reactionID: {
+        type: Schema.Types.ObjectId,
+        default: () => Types.ObjectId()
+    },
+    reactionContent: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+)
 
 module.exports = reactionSchema;
