1525c96dbcedc9daafdb1660d311d1f82c7594c8
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 06:01:18 -0600

    model for thought

--- models/thought.js
+++ models/thought.js
@@ -2,7 +2,22 @@ const {Schema,Types} = require('mongoose');
 const reactionSchema = require("./reaction");
 
 const thoughtSchema = new Schema({
-
-})
+    thoughtContent: {
+        type: String,
+        required: true
+    },
+    username: {
+        type:String,
+        required: true
+    },
+    reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id:false
+    }
+)
 
 module.exports = thoughtSchema;
