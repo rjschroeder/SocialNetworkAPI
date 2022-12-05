35244471094b51efde848318e317cd73b534d688
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 06:23:52 -0600

    set up user routes based on example

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,1 +1,18 @@
 const router = require("express").Router();
+const {getUsers,getSingleUser,createUser,updateUser,deleteUser,addFriend,removeFriend} = require("../../controllers/");
+//once controller is created put name here
+
+router.route("/")
+    .get(getUsers)
+    .post(createUser);
+
+router.route("/:userId")
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route("/:userId/friends/:friendId")
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
