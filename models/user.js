98c29da8160a209f80b03a1fe85e15c45a6ab82c
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 06:09:08 -0600

    model for user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,31 @@
 const {Schema,model} = require("mongoose");
 
 const userSchema = new Schema({
-
-})
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    thoughts: [
+        {type:Schema.Types.ObjectId, ref: "thought"}
+    ],
+    friends: [
+        {type:Schema.Types.ObjectId, ref: "user"}
+    ],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
 
 module.exports = userSchema;
