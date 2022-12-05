71756a20e66c69da25688c119020a97a1700d162
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 06:43:32 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkapi'
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia'
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
