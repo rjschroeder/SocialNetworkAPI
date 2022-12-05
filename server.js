fddec18e49e1d46f07c1a0c84791762ceeaf568a
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 06:38:36 -0600

    express listener

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const app = express();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}!`);
+    })
+})
