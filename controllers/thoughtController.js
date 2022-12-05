37b458c45a2de74b8e5a2594b67aae864a794d03
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 06:52:15 -0600

    added try catch blocks to all controller functions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,25 +2,57 @@ const {Thought, User} = require("../models");
 
 const thoughtController = {
     getThoughts(req, res) {
-
+        try {
+            Thought.find()
+            .sort({createdAt:-1})
+            .then((response) => {
+                res.json(response)
+            })
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
     getSingleThought(req, res) {
-        
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
     createThought(req, res) {
-        
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
     updateThought(req, res) {
-        
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
     deleteThought(req, res) {
-        
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
     addReaction(req, res) {
-        
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
     },
     removeReaction(req, res) {
-        
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
     }
 }
 
